012193fcf55e1fe80d216fa8808ac497725fef95
zhaoyu <zhaoyu@jingnuodata.com>
2020-05-07 17:58:09 +0800

    '加注释'

--- dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -21,9 +21,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Provider {
 
     public static void main(String[] args) throws Exception {
+
         //Prevent to get IPV6 address,this way only work in debug mode
         //But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
         System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
         context.start();
 
